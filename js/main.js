708bf7a12fecc605e57dfd628bdb39d5f27e5d49
techGabrielBr <brgabrieltw@gmail.com>
2021-11-06 15:24:39 -0300

    Implementando Prodto 1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const card = document.querySelector('.card');
+const result = document.querySelector('.result');
 
 document.querySelector('#form').addEventListener('submit', function onSubmit(e){
     e.preventDefault();
@@ -10,24 +10,59 @@ document.querySelector('#form').addEventListener('submit', function onSubmit(e){
     main(searchName);
 });
 
-function main(searchName){
-    const response = getElement(searchName);
-    // const cardContent = createCard(response.results);
+async function main(searchName){
+    const response = await getElement(searchName);
+    const cardContent = createCard(response);
 
-    // card.innerHTML = cardContent;
+    result.innerHTML = cardContent;
 }
 
 const getElement = async (searchName) => {
-    const url = `https://www.tropicalfruitandveg.com/api/tfvjsonapi.php?tfvitem=${searchName}`;
+    const responseTemp = await (await fetch('../json/data.json')).json();
+    const  responseFinal = responseTemp.frutas;
+    let frutaEncontrada =  null;
 
-    const response = await fetch(url)
-    console.log(response)
+    for (const f of responseFinal) {
+        if(searchName.toUpperCase() === f.nome.toUpperCase()){
+            frutaEncontrada = f;
+        }
+    }
+
+    return frutaEncontrada;
 }
 
-function createCard (response){
-    return `
-        <h2> ${response.tfvname} </h2>
-    `
+const createCard = (response) => {
+    if(response === null){
+        return `
+        <div class="card">
+            <h2 class="notFound"> Nenhuma fruta com esse nome foi encontrada em nosso banco de dados.</h2>
+        </div>
+        `
+    }else{
+        return `
+            <div class="card">
+                <h2> ${response.nome}</h2>
+                
+                <div  class="topics">
+                <h4>Descrição</h4>
+                <p>${response.descricao}</p>
+                </div>
+                <img src=${response.imagem} alt="${response.nome}"/>
+                <div class="topics">
+                    <h4>Solo</h4>
+                    <p>${response.solo}</p>
+                </div>
+                <div class="topics">
+                    <h4>Clima</h4>
+                    <p>${response.clima}</p>
+                </div>
+                <div class="topics">
+                    <h4>Propagação</h4>
+                    <p>${response.propagacao}</p>
+                </div>
+        </div>
+        `
+    }
 }
 
 
