7cb82593c57cfd991c75d73e7bbfd0ca32c72f47
techGabrielBr <brgabrieltw@gmail.com>
2021-11-06 12:56:11 -0300

    Teste

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const card = document.querySelector('.card');
+
+document.querySelector('#form').addEventListener('submit', function onSubmit(e){
+    e.preventDefault();
+
+    const input = document.querySelector('#searchName');
+
+    const searchName = input.value;
+
+    main(searchName);
+});
+
+function main(searchName){
+    const response = getElement(searchName);
+    const cardContent = createCard(response.results);
+
+    card.innerHTML = cardContent;
+}
+
+const getElement = async (searchName) => {
+    const url = `http://www.tropicalfruitandveg.com/api/tfvjsonapi.php?tfvitem=${searchName}`;
+
+    const response = await fetch(url)
+    console.log(response)
+}
+
+function createCard (response){
+    return `
+        <h2> ${response.tfvname} </h2>
+    `
+}
+
+
+
